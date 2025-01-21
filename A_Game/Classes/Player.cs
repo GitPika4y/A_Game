1730b9df81e922a681b689d10ac0c168d2dcf826
GitPika4y <169148118+GitPika4y@users.noreply.github.com>
2025-01-21 23:28:10 +0500

    upd 21.01

---
+++ A_Game/Classes/Player.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using  System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shell;
+
+namespace A_Game
+{
+    internal class Player
+    {
+        public Image gameObject => _gameObject;
+        public Vector position => _position;
+
+
+        private Image _gameObject;
+        private Vector _position;
+
+        //Прыжок
+        private static readonly float _gravityDefault = -9.8f;
+        private float _gravity = _gravityDefault;
+        private float _jumpScale = 2;
+        private float _maxJumpForce = 16;
+        private bool isJumping = false;
+
+        //Ходьба
+        private float moveSpeed = 5;
+
+        public Player(ImageSource gameObject, Vector startPosition)
+        {
+            _gameObject = new Image {
+                Source = gameObject,
+                Width = gameObject.Width,
+                Height = gameObject.Height,
+            };
+
+            _position = startPosition;
+        }
+        public void Update(Key? input, Dictionary<Key, bool> holdingInputs)
+        {
+            // Гравитация всегда действует
+            _position.Y += _gravity;
+
+            // Прыжок: если клавиша "Space" нажата и персонаж не находится в состоянии прыжка
+            if (input == Key.Space && !isJumping)
+            {
+                Jump();
+            }
+
+            // Движение влево: клавиша "A" удерживается
+            if (holdingInputs.ContainsKey(Key.A) && holdingInputs[Key.A])
+            {
+                _position.X -= moveSpeed;
+            }
+
+            // Движение вправо: клавиша "D" удерживается
+            if (holdingInputs.ContainsKey(Key.D) && holdingInputs[Key.D])
+            {
+                _position.X += moveSpeed;
+            }
+        }
+        private async void Jump() => await Task.Run(() =>DoJump());
+
+        private async void DoJump()
+        {
+            isJumping = true;
+
+            _gravity = default;//Обнуляем гравитацию
+            while(_gravity < _maxJumpForce)
+            {
+                _gravity += _jumpScale;
+                await Task.Delay(16);
+            }
+            //await Task.Delay(100);
+            while(_gravity > _gravityDefault)
+            {
+                _gravity -= _jumpScale;
+                await Task.Delay(16);
+            }
+            isJumping = false ;
+        }
+    }
+}
---
+++ A_Game/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace A_Game
+{
+    public partial class MainWindow : Window
+    {
+        //ОБъекты
+        private Canvas _canvas;
+        private Player _player;
+        private Vector _canvasCentre;
+        //Доп.Элементы
+        private DispatcherTimer _timer;
+        private Key? _input;
+        private Dictionary<Key, bool> _holdingInputs = new Dictionary<Key, bool>()
+            {   
+                [Key.D] = false,
+                [Key.A] = false
+            };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void InitializeObjects()
+        {
+            _player = new Player(GetImage("D:/asesprite_Img/My first Game(Dream)/Img/Player_Idle.png"),
+                _canvasCentre);
+            Canvas.Children.Add(_player.gameObject);
+
+
+            UpdateCanvas();
+        }
+        private void InitializeTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(16);// ~ 60 FPS
+            _timer.Tick += Update;
+            _timer.Start();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Инициализировать центр Canvas,объекты,таймер после загрузки окна
+            UpdateCanvasCentre();
+            InitializeObjects();
+            InitializeTimer();
+        }
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Обновлять центр Canvas при изменении его размеров
+            _canvas = Canvas;
+            UpdateCanvasCentre();
+        }
+
+
+
+        private void Update(object sender, EventArgs e)
+        {
+            UpdateCanvas();
+            _player.Update(_input, _holdingInputs);
+
+            _input = null;
+        }
+        private void UpdateCanvas()
+        {
+            // Устанавливаем координаты игрока так, чтобы он оказался по центру Canvas
+            Canvas.SetLeft(_player.gameObject, _player.position.X);
+            Canvas.SetBottom(_player.gameObject, _player.position.Y);
+        }
+        private void UpdateCanvasCentre()
+        {
+            // Убедитесь, что размеры доступны
+            if (_canvas.ActualWidth > 0 && _canvas.ActualHeight > 0)
+            {
+                _canvasCentre = new Vector(_canvas.ActualWidth / 2, _canvas.ActualHeight / 2);
+            }
+        }
+
+
+        private ImageSource GetImage(string imgPath)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imgPath, UriKind.Absolute); // Путь к файлу
+            bitmap.EndInit();
+            return bitmap;
+        }
+        private void InputGetKeyDown(object sender, KeyEventArgs e)
+        {
+            _input = e.Key;
+            if (_holdingInputs.ContainsKey(e.Key))
+            {
+                _holdingInputs[e.Key] = true;
+            }
+        }
+        private void InputGetKeyUp(object sender, KeyEventArgs e)
+        {
+            _input = e.Key;
+            if (_holdingInputs.ContainsKey(e.Key))
+            {
+                _holdingInputs[e.Key] = false;
+            }
+        }
+
+
+
+
+
+
+    }
+}
